7ba1d86c945803a71b6b72d59683a4e268cb6bab
Luis Javier Lozoya <luisloart@gmail.com>
2020-10-28 20:18:10 -0400

    Updates

---
+++ precache-manifest.bcd4334c97c143ef4e4173a44f2e77f6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9016a31a9650a44ab78eb75f75f06ef3",
+    "url": "/Rental-App/index.html"
+  },
+  {
+    "revision": "b663f581b30829df561b",
+    "url": "/Rental-App/static/css/main.82ffe315.chunk.css"
+  },
+  {
+    "revision": "49ef666d9f109ea57d00",
+    "url": "/Rental-App/static/js/2.33658b2e.chunk.js"
+  },
+  {
+    "revision": "486fd1348cd78357e1153b1205385055",
+    "url": "/Rental-App/static/js/2.33658b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b663f581b30829df561b",
+    "url": "/Rental-App/static/js/main.c80a9bfc.chunk.js"
+  },
+  {
+    "revision": "61e8e6661870cdb5360b",
+    "url": "/Rental-App/static/js/runtime-main.6f4282c4.js"
+  },
+  {
+    "revision": "f343ffa5e994d46418f5ea618dbb1827",
+    "url": "/Rental-App/static/media/Bike1.f343ffa5.jpg"
+  },
+  {
+    "revision": "31cbe06239845cd5459a2d1779ed6463",
+    "url": "/Rental-App/static/media/Bike3.31cbe062.jpg"
+  },
+  {
+    "revision": "18d65fccf06d08ae69d4bd65d7f685c8",
+    "url": "/Rental-App/static/media/Kayak.18d65fcc.jpg"
+  },
+  {
+    "revision": "f14ab2d4b922d4721d2cb0c05edcb404",
+    "url": "/Rental-App/static/media/Kayak2.f14ab2d4.jpg"
+  },
+  {
+    "revision": "76480b21b3d75e07dad04d662c2a66c6",
+    "url": "/Rental-App/static/media/LogIn.76480b21.jpg"
+  },
+  {
+    "revision": "d12778bdd231e8ed1fa2d1936b3cf51c",
+    "url": "/Rental-App/static/media/Logo.d12778bd.png"
+  },
+  {
+    "revision": "6884588dd6b1f168d89555f3d4d75763",
+    "url": "/Rental-App/static/media/SignUp2.6884588d.jpg"
+  },
+  {
+    "revision": "e0a8d50eacaefd94bbf791a6e99adac8",
+    "url": "/Rental-App/static/media/Surf1.e0a8d50e.jpg"
+  },
+  {
+    "revision": "61c670e9fe4f89ae786b9be6f6378323",
+    "url": "/Rental-App/static/media/Surf2.61c670e9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Rental-App/Home/precache-manifest.ab7a223c7ab2af669aef299f1d66473a.js"
+  "/Rental-App/precache-manifest.bcd4334c97c143ef4e4173a44f2e77f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rental-App/Home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rental-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
